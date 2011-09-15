74ec1f96616e1b3ec7350655b629456957769f41
Chris Brown <chrisbrown.guitar@googlemail.com>
2011-09-15 14:59:01 +0100

    Added Static Semantics

--- Parser/Parse.hs
+++ Parser/Parse.hs
@@ -15,8 +15,8 @@ import Debug.Trace
 type Prog = [ Defn ]
 
 
-data Expr = Ident String
-          | Con String [ Expr ]
+data Expr = Ident String SourcePos SourcePos
+          | Con String SourcePos SourcePos [ Expr ]
           | App Expr Expr
           | Lit Literal
     deriving Show
@@ -31,22 +31,24 @@ data Defn = Fun Name [ Pat ] Expr
           | Dat TyName [ Constr ]
     deriving Show
 
-data Pat = PatIdent String
+data Pat = PatIdent String SourcePos SourcePos
+         | PatConstr String SourcePos SourcePos [ Pat ]
     deriving Show
 
-data Name = DefIdent String
+data Name = DefIdent String SourcePos SourcePos
     deriving Show
 
-data TyName = TyIdent String
+data TyName = TyIdent String SourcePos SourcePos
     deriving Show
 
-data Constr = ConName String [ String ]
+data Constr = ConName String SourcePos SourcePos [ String ]
     deriving Show
 
 
+-----------------------------------------------------------------------------------------------------
 
 applyCon :: Expr -> Expr
-applyCon (App (applyCon -> Con s as) e) = Con s (as ++ [applyCon e])
+applyCon (App (applyCon -> Con s s1 s2 as) e) = Con s s1 s2 (as ++ [applyCon e])
 applyCon (App (applyCon -> l) e) = App l (applyCon e)
 applyCon e = e
 
@@ -62,6 +64,8 @@ languageDef =
                                           , "data"
                                           ]
              ,  Token.reservedOpNames   = [ "=" 
+                                          , "("
+                                          , ")"
                                           ]
             }
 
@@ -78,6 +82,8 @@ reservedOp = Token.reservedOp lexer
 stringLiteral = Token.stringLiteral lexer
 charLiteral = Token.charLiteral lexer
 
+-----------------------------------------------------------------------------------------------------
+
 whileParser :: Parser Prog
 whileParser = do 
     i <- many datDefinition 
@@ -85,13 +91,13 @@ whileParser = do
     return (i++l)
 
 funDefinition :: Parser Defn
-funDefinition = Fun <$> (DefIdent <$> identifier) <*> many pat <*> (reservedOp "=" >> applyCon <$> expression =>> semi)
+funDefinition = Fun <$> funName <*> many pat <*> (reservedOp "=" >> applyCon <$> expression =>> semi)
 
 datDefinition :: Parser Defn
-datDefinition = Dat <$> (reserved "data" >> TyIdent <$> up) <*> (reservedOp "=" >> sepBy1 constr (char '|' ) =>> semi)
+datDefinition = Dat <$> (reserved "data" >> typ ) <*> (reservedOp "=" >> sepBy1 constr (char '|' ) =>> semi)
 
 constr :: Parser Constr
-constr = ConName <$> (whiteSpace >> up) <*> (many identifier )
+constr = ConName <$> (whiteSpace >> up) <*> getPosition <*> getPosition <*> (many identifier) 
 
 literal :: Parser Literal
 literal =  (Str <$> stringLiteral)
@@ -106,15 +112,52 @@ up = identifier >>= \i -> when (isLower $ head i) (fail "Constructor") >> return
 m =>> m' = m >>= \v -> m' >> return v
 
 pat :: Parser Pat
-pat = do pat <- identifier
-         return (PatIdent pat)
+pat = patIdent
+  <|> patConstr
+
+patIdent :: Parser Pat
+patIdent = do pos <- getPosition
+              ident <- identifier
+              return (PatIdent ident pos pos)
+
+
+patConstr :: Parser Pat
+patConstr = do
+                p1 <- optionMaybe (reservedOp "(")
+                pos <- getPosition
+                i  <- up
+                
+                pats <- many pat
+                case pats of 
+                    [] -> do
+                            case p1 of
+                                Nothing -> return (PatConstr i pos pos [])
+                                Just x  -> do
+                                             reservedOp ")"
+                                             return (PatConstr i pos pos [])
+                    (x:xs) -> do
+                                case p1 of
+                                    Nothing -> fail "Missing ( on pattern"
+                                    Just g  -> do 
+                                                    reservedOp ")"
+                                                    return (PatConstr i pos pos (x:xs))
+
+
+typ :: Parser TyName
+typ = do
+        pos <- getPosition
+        u <- up
+        return (TyIdent u pos pos)
+
 
 funName :: Parser Name
-funName = do var <- identifier
-             return (DefIdent var)
+funName = do
+            pos <- getPosition
+            ident <- identifier
+            return (DefIdent ident pos pos)
 
 expression :: Parser Expr
-expression = applify1 <$> many1 (parens expression <|> constructor <|> variable <|> ( Lit <$>  literal))
+expression = applify1 <$> many1 (parens expression <|> constructor <|> variable <|> (Lit <$>  literal))
                     
 
 applify1 (v:vs) = applify v vs 
@@ -123,12 +166,13 @@ applify a [] = a
 applify a (v:vs) = applify (App a v) vs
 
 constructor :: Parser Expr
-constructor = try (identifier >>= \i -> when (isLower $ head i) (fail "Constructor") >> return (Con i []))
+constructor = try (getPosition >>= \s -> identifier >>= \i -> when (isLower $ head i) (fail "Constructor") >> return (Con i s s []))
 
 variable :: Parser Expr
 variable = do
-                var <- identifier
-                return (Ident var)
+              pos <- getPosition
+              ident <- identifier
+              return (Ident ident pos pos)
          
 parseString :: String -> Prog
 parseString str =
@@ -141,4 +185,134 @@ parseFile file =
   do program  <- readFile file
      case parse whileParser "" program of
        Left e  -> print e >> fail "parse error"
-       Right r -> return r
+       Right r -> do
+                     -- print r 
+                     let (_, _, t) = traverse [] [] r 
+                     return t
+
+
+-------------------------------------------------------------------------------------------------
+--
+-- build a stack tracer that resolves bindings in the tree
+--
+-------------------------------------------------------------------------------------------------
+{-
+data Expr = Ident String SourcePos SourcePos
+          | Con String SourcePos SourcePos [ Expr ]
+          | App Expr Expr
+          | Lit Literal
+    deriving Show
+
+data Literal = Str String 
+             | In Integer
+             | Ch Char
+    deriving Show
+
+
+data Defn = Fun Name [ Pat ] Expr
+          | Dat TyName [ Constr ]
+    deriving Show
+
+data Pat = PatIdent String SourcePos SourcePos
+         | PatConstr String SourcePos SourcePos [ Pat ]
+    deriving Show
+
+data Name = DefIdent String SourcePos SourcePos
+    deriving Show
+
+data TyName = TyIdent String SourcePos SourcePos
+    deriving Show
+
+data Constr = ConName String SourcePos SourcePos [ String ]
+    deriving Show
+
+-}
+
+type LocalEnv = [ (String, SourcePos) ]
+
+type GlobalEnv = [ (String, SourcePos) ]
+
+
+traverse :: GlobalEnv -> LocalEnv -> Prog -> (GlobalEnv, LocalEnv, Prog)
+traverse genv lenv [] = (genv, lenv, [])
+traverse genv lenv (d:ds) = (genv2, lenv2, def:defs)
+        where  (genv1, lenv1, def) = traverseDefn genv [] d 
+               (genv2, lenv2, defs) = traverse genv1 lenv1 ds 
+
+traverseDefn :: GlobalEnv -> LocalEnv -> Defn -> (GlobalEnv, LocalEnv, Defn)
+traverseDefn genv lenv (Dat n cons) = dat
+    where
+        dat = (genv3, [], Dat n cons)
+        (genv3, [], newCons) = traverseCons genv [] cons
+
+traverseDefn genv lenv (Fun n pats e) = defn
+    where
+        genv1 = pushNameStack n genv
+        (genv2, lenv2, pats2) = traversePats genv1 lenv pats
+        defn = (genv2, lenv2, Fun n pats2 newE)
+        newE = fixBindings e lenv2 genv2
+
+
+traverseCons :: GlobalEnv -> LocalEnv -> [ Constr ] -> (GlobalEnv, LocalEnv, [ Constr ])
+traverseCons genv lenv [] = (genv, lenv, [])
+traverseCons genv lenv (c:cs)
+    = traverseCons genv1 [] cs
+    where
+        genv1 = pushConStack c genv
+
+    
+traversePats :: GlobalEnv -> LocalEnv -> [ Pat ] -> (GlobalEnv, LocalEnv, [ Pat ])
+traversePats genv lenv [] = (genv, lenv, [])
+traversePats genv lenv (p:ps) = (genv2, lenv2, p:pats)
+        where
+            lenv1 = case p of
+                      (PatIdent n s1 s2) ->  pushPatStack p lenv
+                      (PatConstr n s1 s2 pats') -> pushPatsStack pats' lenv
+            (genv2, lenv2, pats) = traversePats genv lenv1 ps
+
+pushPatStack :: Pat -> LocalEnv -> LocalEnv
+pushPatStack (PatIdent n s1 s2) env = (n, s1) : env
+
+pushPatsStack :: [ Pat ] -> LocalEnv -> LocalEnv
+pushPatsStack [] env = env
+pushPatsStack ((PatConstr n s1 s2 pats):ps) env = pushPatsStack ps (pushPatsStack pats env)
+pushPatsStack ((PatIdent n s1 s2):ps) env = pushPatsStack ps ((n, s1) : env)
+
+pushNameStack :: Name -> GlobalEnv -> GlobalEnv
+pushNameStack (DefIdent n s1 s2) env = (n, s1) : env
+
+pushDatNameStack :: TyName -> GlobalEnv -> GlobalEnv
+pushDatNameStack (TyIdent n s1 s2) env = (n, s1) : env
+
+pushConStack :: Constr -> GlobalEnv -> GlobalEnv
+pushConStack (ConName n s1 s2 ts) env = (n, s1) : env
+
+fixBindings :: Expr -> LocalEnv -> GlobalEnv -> Expr
+fixBindings l@(Lit _) _ _ = l
+fixBindings (App e1 e2) l g = App (fixBindings e1 g l) (fixBindings e2 g l)
+fixBindings (Ident n s1 s2) l g = Ident n s1' s2
+    where
+        s1' = case checkEnv n l g of
+                Nothing -> error ("Identifier: " ++ n ++ " is not bound!")
+                Just x  -> x
+fixBindings (Con n s1 s2 es) l g = Con n s1' s2 es'
+    where
+        s1' = case checkEnv n [] g of
+                Nothing -> error ("Constructor: " ++ n ++ " is not bound!" ++ (show l))
+                Just x  -> x
+        es' = mapBinds es l g
+        mapBinds [] l g = []
+        mapBinds (e:es) l g = fixBindings e l g : mapBinds es l g
+    
+
+checkEnv :: String -> LocalEnv -> GlobalEnv -> Maybe SourcePos
+checkEnv n locals globals 
+  = case n `inEnv` locals of
+        Nothing -> n `inEnv` globals
+        Just x  -> Just x
+
+inEnv :: String -> [ (String, SourcePos) ] -> Maybe SourcePos
+inEnv _ [] = Nothing
+inEnv n ( (n2, s):xs) 
+ | n == n2 = Just s 
+ | otherwise = inEnv n xs
